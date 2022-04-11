5f99e4adfd26aaedf3a03dbe15ef9779ceca5a74
Artur <Artur.Descheemaecker@UGent.be>
2022-04-11 18:42:25 +0200

    x

--- Simulation Module/src/Main.java
+++ Simulation Module/src/Main.java
@@ -1,21 +1,8 @@
 public class Main {
 
-    /**
-     //
-     //  main.cpp
-     //  projestSMA
-     //
-     //  Created by Tine Meersman on 09/02/2022.
-     //  Copyright © 2022 Tine Meersman. All rights reserved.
-     //
-
-     #include <iostream>
-
-     #include "Simulation.hpp"
-
-     int main(int argc, const char * argv[]) {
-     simulation *sim = new simulation;
-     sim->runSimulations();
+     public static void main(String args[]){
+         Simulation sim = new Simulation();
+         sim.runSimulations();
      }
-     */
+
 }
--- Simulation Module/src/Slot.java
+++ Simulation Module/src/Slot.java
@@ -1,7 +1,7 @@
 public class Slot {
-    double startTime;  // start time of the slot (in hours)
-    double appTime;    // appointment time of the slot, dependant on type and rule (in hours)
-    int slotType;       // type of slot (0=none, 1=elective, 2=urgent within normal working hours, 3=urgent in overtime)
-    int patientType;    // (0=none, 1=elective, 2=urgent)
+    public double startTime;  // start time of the slot (in hours)
+    public double appTime;    // appointment time of the slot, dependant on type and rule (in hours)
+    public int slotType;       // type of slot (0=none, 1=elective, 2=urgent within normal working hours, 3=urgent in overtime)
+    public int patientType;    // (0=none, 1=elective, 2=urgent)
 
 }
