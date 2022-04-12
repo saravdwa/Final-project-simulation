75218dc4ee5b0012d2f42169a3000267f0adfbde
Artur <Artur.Descheemaecker@UGent.be>
2022-04-12 09:07:26 +0200

    x

--- Simulation Module/src/Patient.java
+++ Simulation Module/src/Patient.java
@@ -1,21 +1,23 @@
+import static java.lang.StrictMath.max;
+
 public class Patient {
     // object class
-    int nr;
-    int patientType;    // (0=none, 1=elective, 2=urgent)
-    int scanType;       // elective: (0=none), urgent: (0=brain, 1=lumbar, 2=cervival, 3=abdomen, 4=others)
-    int callWeek;       // week of arrival (elective: call, urgent: actual arrival)
-    int callDay;        // day of arrival (elective: call, urgent: actual arrival)
-    double callTime;    // time of arrival (elective: call, urgent: actual arrival) (in hours)
-    int scanWeek;       // week of appointment
-    int scanDay;        // day of appointment
-    int slotNr;         // slot number of appointment
-    double appTime;     // time of appointment (elective: according to rule, urgent: slot start time) (in hours)
-    double tardiness;   // (in hours)
-    boolean isNoShow;
-    double scanTime;    // actual start time of the scan
-    double duration;    // actual duration of the scan
+    public int nr;
+    public int patientType;    // (0=none, 1=elective, 2=urgent)
+    public int scanType;       // elective: (0=none), urgent: (0=brain, 1=lumbar, 2=cervival, 3=abdomen, 4=others)
+    public int callWeek;       // week of arrival (elective: call, urgent: actual arrival)
+    public int callDay;        // day of arrival (elective: call, urgent: actual arrival)
+    public double callTime;    // time of arrival (elective: call, urgent: actual arrival) (in hours)
+    public int scanWeek;       // week of appointment
+    public int scanDay;        // day of appointment
+    public int slotNr;         // slot number of appointment
+    public double appTime;     // time of appointment (elective: according to rule, urgent: slot start time) (in hours)
+    public double tardiness;   // (in hours)
+    public boolean isNoShow;
+    public double scanTime;    // actual start time of the scan
+    public double duration;    // actual duration of the scan
 
-    Patient(int nr_, int patientType_, int scanType_, int callWeek_, int callDay_, double callTime_, double tardiness_, boolean isNoShow_, double duration_){
+    public Patient(int nr_, int patientType_, int scanType_, int callWeek_, int callDay_, double callTime_, double tardiness_, boolean isNoShow_, double duration_){
         nr = nr_;
         patientType = patientType_;
         scanType = scanType_;
@@ -34,16 +36,16 @@ public class Patient {
         scanTime = -1.0;
     }
 
-    double getAppWT(){
+    public double getAppWT(){
         if(slotNr != -1){
             return (double)(((scanWeek-callWeek)*7 + scanDay - callDay)*24 + appTime - callTime); // in hours
         }else{
-            printf("CAN NOT CALCULATE APPOINTMENT WT OF PATIENT %d", nr);
-            exit(1);
+            System.out.println("CAN NOT CALCULATE APPOINTMENT WT OF PATIENT %d" + nr);
+            System.exit(1);
         }
     }
 
-    double getScanWT(){
+    public double getScanWT(){
         if(scanTime != 0){
             double wt = 0;
             if(patientType == 1){ // elective
@@ -53,8 +55,8 @@ public class Patient {
             }
             return max(0.0,wt);
         }else{
-            printf("CAN NOT CALCULATE SCAN WT OF PATIENT %d", nr);  // in hours
-            exit(1);
+            System.out.println("CAN NOT CALCULATE SCAN WT OF PATIENT %d" + nr);  // in hours
+            System.exit(1);
         }
     }
 
