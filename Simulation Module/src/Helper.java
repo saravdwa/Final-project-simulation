c00a7a7441c81b6632747429947c419b364d5f94
Artur <Artur.Descheemaecker@UGent.be>
2022-04-12 08:52:07 +0200

    x

--- Simulation Module/src/Helper.java
+++ Simulation Module/src/Helper.java
@@ -1,8 +1,16 @@
+import java.util.Random;
+
+import static java.lang.StrictMath.*;
+
+
 public class Helper {
     public double exponential_distribution(double lambda){
-        float j1 = float (rand()%1000)/1000;
-        if (j1 == 0) j1 += 0.0001;
-        float j2 = -log(j1)/lambda;
+        Random r = null;
+        float j1 = (float) r.nextInt(1000+1)/1000;
+        if (j1 == 0) {
+            j1 += 0.0001;
+        }
+        float j2 = (float) (-log(j1)/lambda);
         return j2;
     }
 
@@ -10,12 +18,13 @@ public class Helper {
     public int poisson_distribution(double lambda){
         float k, L, j1, j2, j3;
         int p;
-        j1 = float (rand()%1000)/1000;
+        Random r = null;
+        j1 = (float) r.nextInt(1000+1)/1000;
         k = 0;
-        L = exp(-lambda);
+        L = (float) exp(-lambda);
         j3 = 0;
         do{
-            j2 = L * pow(lambda, k);
+            j2 = (float) (L * pow(lambda, k));
             p = 1;
             for (int i6 = 0; i6 <= k; i6++){
                 if (i6 == 0)
@@ -28,7 +37,7 @@ public class Helper {
             k++;
         } while (j1 >= j3);
 
-        return k-1;
+        return (int) (k-1);
     }
 
     public int normal_distribution(double mean, double stdev){
@@ -37,22 +46,24 @@ public class Helper {
         float v1, v2, t;
         int x;
         do{
-            v1 = float (rand()%1000)*2;
+            Random r = null;
+            v1 = (float) r.nextInt(1000+1)*2;
             v1 /= 1000;
             v1 -= 1;
-            v2 = float (rand()%1000)*2;
+            v2 = (float) r.nextInt(1000+1)*2;
             v2 /= 1000;
             v2 -= 1;
             t=v1*v1+v2*v2;
         }
         while(t>=1||t==0);
-        float multiplier = sqrt(-2*log(t)/t);
-        x = v1 * multiplier * stdev + mean;
+        float multiplier = (float) sqrt(-2*log(t)/t);
+        x = (int) (v1 * multiplier * stdev + mean);
         return x;
     }
 
     public int bernouilli_distribution(double prob){
-        float j1 = rand()%1000/1000;
+        Random r = null;
+        float j1 = (float) r.nextInt(1000+1)/1000;
         if (j1 < prob)
             return 0;
         else
@@ -60,8 +71,9 @@ public class Helper {
     }
 
     public int uniform_distribution(double a, double b){
-        float j1 = rand()%1000/1000;
-        return (int) a + (b-a) * j1;
+        Random r = null;
+        float j1 = (float) r.nextInt(1000+1)/1000;
+        return (int) ((int) a + (b-a) * j1);
     }
 
 
@@ -69,19 +81,20 @@ public class Helper {
         float mean, stdev,x, L;
 
         mean = (a+b+c)/3;
-        stdev = (pow(a,2)+pow(b,2)+pow(c,2)-a*b-a*c-b*c)/18;
-        stdev = sqrt(stdev);
-        float j1 = float (rand()%1000)/1000;
+        stdev = (float) ((pow(a,2)+pow(b,2)+pow(c,2)-a*b-a*c-b*c)/18);
+        stdev = (float) sqrt(stdev);
+        Random r = null;
+        float j1 = (float) r.nextInt(1000+1)/1000;
         x = a;
 
         do{
             if (x <= b)
-                L = pow((x-a),2)/((c-a)*(b-a));
+                L = (float) (pow((x-a),2)/((c-a)*(b-a)));
             else
-                L = 1-(pow(c-x,2)/((c-a)*(c-b)));
+                L = (float) (1-(pow(c-x,2)/((c-a)*(c-b))));
             x++;
         } while (j1 >= L);
 
-        return x-1;
+        return (int) (x-1);
     }
 }
