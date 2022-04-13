4c7ea8c1bd161f80bd10332e21685aaf61c54a2b
MartheRoos <martheroos.deruysscher@ugent.be>
2022-04-13 17:34:06 +0200

    Update Main.java

--- Simulation Module/src/Main.java
+++ Simulation Module/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
      public static void main(String args[]){
-         Simulation sim = new Simulation();
+         Simulation sim = new Simulation(); //test
          sim.runSimulations();
      }
 
